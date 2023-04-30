9a02f6a07f892446c1af3eb40fba1ae3ca88e36c
MATHYMALAR K V <56343100+mathymalar@users.noreply.github.com>
2023-04-30 12:36:58 +0530

    Add files via upload

---
+++ FibonacciSeries.java
@@ -0,0 +1,16 @@
+package week1.day1;
+
+public class FibonacciSeries {
+	public static void main(String[] args) {
+		 int n1=0,n2=1,n3; 
+		 System.out.println(n1+" "+n2);
+		 for(int i=2; i<11; i++) 
+		 {
+		 	 n3=n1+n2; 
+		 	
+		 	System.out.println(" "+n3);
+			  n1=n2;  
+			  n2=n3;  
+		 }
+	}
+}
